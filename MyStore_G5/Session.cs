a492cb7a7c7a21550f07e65bd63493bd3f0ad664
vuhuy301 <danhhuy8b@gmail.com>
2024-05-15 20:17:12 +0700

    update login

---
+++ MyStore_G5/Session.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStore_G5
+{
+    internal class Session
+    {
+        public static string? Username { get; set; } = null;
+    }
+}

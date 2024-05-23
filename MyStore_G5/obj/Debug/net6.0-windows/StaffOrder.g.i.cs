15b3f2260da9a70c2be8b5409b17f1ec0abaccd9
ioaHmaN <98122963+namlnh10@users.noreply.github.com>
2024-05-23 11:45:01 +0700

    Save changes before merge

--- MyStore_G5/obj/Debug/net6.0-windows/StaffOrder.g.i.cs
+++ MyStore_G5/obj/Debug/net6.0-windows/StaffOrder.g.i.cs
@@ -55,13 +55,13 @@ namespace MyStore_G5 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.5.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MyStore_G5;component/stafforder.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MyStore_G5;V1.0.0.0;component/stafforder.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\StaffOrder.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -71,7 +71,7 @@ namespace MyStore_G5 {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.5.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
